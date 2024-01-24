2ef2b93663d084afa5d221007f139808211f9fee
Ayrton S. Machado <ayrtonsoaresm@gmail.com>
2024-01-24 19:51:43 -0300

    link all pages and add a route in url to markdowns

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
+def entry(request, title):
+    entry_md = util.get_entry(title)
+    entry_HTML = Markdown().convert(entry_md)
+    return render(request, "encyclopedia/entry.html", {
+        'entry': entry_HTML,
+        'title': title
+    })
