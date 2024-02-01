20d8f0672ba527bc891b3af9ea08dd11524fb129
Ayrton Machado <ayrtonsoaresm@gmail.com>
2024-02-01 18:32:16 -0300

    create semi-working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/", views.index, name="index"),
     path('wiki/<str:title>', views.entry, name='entry'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    path('create', views.create, name='create')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -11,6 +11,14 @@ class searchForm(forms.Form):
     'style': 'width 100%'
     }))
 
+class CreatePage(forms.Form):
+    createTitle = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'placeholder': 'Title',
+    }))
+    createContent = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'placeholder': 'Content',
+    }))
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
@@ -19,6 +27,11 @@ def index(request):
 
 def entry(request, title):
     entry_md = util.get_entry(title)
+    if entry_md is None:
+        return(render(request, 'encyclopedia/error.html',{
+            'title': title,
+            "form": searchForm()
+        }))
     entry_HTML = Markdown().convert(entry_md)
     return render(request, "encyclopedia/entry.html", {
         "entries": util.list_entries(),
@@ -42,11 +55,29 @@ def search(request):
                 if text_field.lower() in entry.lower():
                     entries_found.append(entry)
             return render(request, 'encyclopedia/search.html', {
+                "entries": util.list_entries(),
                 'results': entries_found,
                 'text_field': text_field,
                 'form': searchForm()
             })
     return render(request, 'encyclopedia/search.html', {
         'text_field': '',
-        "form": SearchForm()
+        "form": searchForm()
+    })
+
+def create(request):
+    if request.method == 'POST':
+        create_title = CreatePage(request.POST)
+        create_content = CreatePage(request.POST)
+        if create_title.is_valid():
+            if create_content.is_valid():
+                title = create_title.cleaned_data['createTitle']
+                content = create_content.cleaned_data['createContent']
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse('entry', args=[title]))
+    return render(request, 'encyclopedia/create.html', {
+        "entries": util.list_entries(),
+        'form': searchForm(),
+        'title_form': CreatePage(),
+        'content_form': CreatePage()
     })
